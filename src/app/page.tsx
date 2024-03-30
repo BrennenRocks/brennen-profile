7e9a22d525d9bdcc155fffe850fd3d0121b592a0
Brennen Davis <brennendavis@gmail.com>
2024-03-30 00:32:44 -0600

    all text

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -3,40 +3,270 @@ import Image from "next/image";
 
 export default function Home() {
   return (
-    <main className="flex min-h-screen flex-col items-center justify-between py-12">
-      <div className="flex space-x-6 w-full max-w-4xl">
-        <div>
-          <Image
-            src="/me.jpg"
-            alt="me"
-            height={128}
-            width={128}
-            className="fixed z-10 rounded-3xl"
-          />
-          <div className="bg-violet-400 opacity-30 -translate-y-1 -translate-x-1 h-[128px] w-[136px] rounded-3xl"></div>
+    <main className="flex min-h-screen flex-col items-center py-12">
+      <div className="flex flex-col gap-4 w-full max-w-3xl">
+        <div className="flex space-x-6">
+          <div className="relative">
+            <Image
+              src="/me.jpg"
+              alt="me"
+              height={128}
+              width={128}
+              className="absolute z-10 rounded-3xl w-auto h-auto"
+            />
+            <div className="bg-violet-400 opacity-30 -translate-y-1 -translate-x-1 h-[128px] w-[136px] rounded-3xl"></div>
+          </div>
+          <div className="flex flex-col space-y-4">
+            <h1 className="font-bold text-6xl">Brennen Davis</h1>
+            <div className="flex space-x-12">
+              <a
+                className="text-gray-300 pointer-events-none flex place-items-center gap-2 p-8 lg:pointer-events-auto lg:p-0 hover:text-gray-50"
+                href="https://github.com/BrennenRocks"
+                target="_blank"
+                rel="noreferrer"
+              >
+                <GithubIcon />
+                <div>Github</div>
+              </a>
+              <a
+                className="text-gray-300 pointer-events-none flex place-items-center gap-2 p-8 lg:pointer-events-auto lg:p-0 hover:text-gray-50"
+                href="https://twitter.com/Elevated_SW"
+                target="_blank"
+                rel="noreferrer"
+              >
+                <XIcon />
+                <div>X (Twitter)</div>
+              </a>
+            </div>
+          </div>
         </div>
-        <div className="flex flex-col space-y-4">
-          <h1 className="font-bold text-6xl">Brennen Davis</h1>
-          <div className="flex space-x-12">
+        <div className="flex flex-col leading-relaxed gap-2 w-10/12 m-auto text-gray-200">
+          <p className="text-lg text-gray-50">👋 Hello there Tailwind team!</p>
+          <p>
+            My name is Brennen and I hope to <em>wow</em> you with some projects
+            I&apos;ve worked on and my infectious enthusiasm to get things
+            right.
+          </p>
+          <h3 className="text-xl py-2 text-gray-50">For Starters</h3>
+          <p>
+            My programming fundamentals started in the world of Java as I worked
+            on getting my CS Degree and my first programming job. This really
+            helped me to get a strong foundation in working with a fully typed
+            language and the concepts of OOP. Many people will scoff at the idea
+            of someone actually enjoying Java but I had and have no qualms with
+            it!
+          </p>
+          <p>
+            My first product was an on premise desktop app with an outdated
+            external API. I saw this as an opportunity to learn JavaScript and
+            TypeScript and dive into more of the web world with my project to
+            build a REST API wrapper around the desktop API.
+          </p>
+          <a
+            className="pointer-events-none lg:pointer-events-auto hover:text-gray-50 underline"
+            href="https://github.com/BrennenRocks/fishbowl-js"
+            target="_blank"
+            rel="noreferrer"
+          >
+            Fishbowl-js
+          </a>
+          <p>
+            Don&apos;t judge too harshly as I was but a wee Jr Dev. This is
+            where I got my open-source chops and a taste for creating tools to
+            help other developers. At Fishbowl we also wrote code as a&nbsp;
             <a
-              className="text-gray-300 pointer-events-none flex place-items-center gap-2 p-8 lg:pointer-events-auto lg:p-0 hover:text-gray-50"
-              href="https://github.com/BrennenRocks"
+              className="pointer-events-none lg:pointer-events-auto hover:text-gray-50 underline"
+              href="https://www.agilealliance.org/glossary/mob-programming/"
               target="_blank"
               rel="noreferrer"
             >
-              <GithubIcon />
-              <div>Github</div>
+              mob
             </a>
+            &nbsp;everyday. This solidified my knowledge of the strenghts of
+            pair programming. I probably wouldn&apos;t do mobbing every day
+            though...
+          </p>
+          <h3 className="text-xl py-2 text-gray-50">The Journey</h3>
+          <p>
+            I love to learn about new technologies. I follow many Tech
+            &quot;Influencers&quot; on YouTube and Twitter just to try to stay
+            up to date. This led me to learning about Tailwind CSS v1 and Chakra
+            UI. Comparing the two I found that Chakra UI was easier for me at
+            the time and this is honestly where I learned most of my CSS
+            knowledge. I loved how the CSS properties were props in the React
+            Components.
+          </p>
+          <p className="text-sm pt-2 text-gray-50">Side Note</p>
+          <div>
+            <em className="text-sm">
+              Adam, I did see your tweet about Robin&apos;s application and read
+              through it. I must apologize to Robin that I enjoy Chakra UIs
+              string-based API. Huge props to&nbsp;
+              <a
+                className="pointer-events-none lg:pointer-events-auto hover:text-gray-50 underline"
+                href="https://github.com/segunadebayo"
+                target="_blank"
+                rel="noreferrer"
+              >
+                Segun Adebayo
+              </a>
+              &nbsp;and team for maintaining string-based APIs
+            </em>
+            &nbsp;😛
+          </div>
+          <p>
+            I also learned about NextJS and the cool ways to generate static
+            HTML pages with data from a database. At this same time Age of
+            Empires 4 was coming out. As I was playing early releases of Age of
+            Empires 4 I wanted to put my newfound Chakra UI and NextJS to the
+            test.
+          </p>
+          <div>
             <a
-              className="text-gray-300 pointer-events-none flex place-items-center gap-2 p-8 lg:pointer-events-auto lg:p-0 hover:text-gray-50"
-              href="https://twitter.com/Elevated_SW"
+              className="pointer-events-none lg:pointer-events-auto hover:text-gray-50 underline"
+              href="https://taupe-gingersnap-1b60d9.netlify.app/"
               target="_blank"
               rel="noreferrer"
             >
-              <XIcon />
-              <div>X (Twitter)</div>
+              AOE Build Orders
+            </a>
+            &nbsp;
+            <a
+              className="pointer-events-none lg:pointer-events-auto hover:text-gray-50 underline"
+              href="https://github.com/Elevated-Software/aoe-build-order"
+              target="_blank"
+              rel="noreferrer"
+            >
+              (Link to Code)
             </a>
           </div>
+          <h3 className="text-xl py-2 text-gray-50">
+            Lease End <span className="text-sm">(my current 9-5)</span>
+          </h3>
+          <p>
+            The AOE Build Orders project actually was a big help to get a job at
+            my current company,&nbsp;
+            <a
+              className="pointer-events-none lg:pointer-events-auto hover:text-gray-50 underline"
+              href="https://www.leaseend.com/"
+              target="_blank"
+              rel="noreferrer"
+            >
+              Lease End
+            </a>
+            . While I&apos;ve been here I have been able to help other
+            developers in many ways. I thoroughly enjoy writing TypeScript and
+            one of my first refactors that I participated in was updating all of
+            our models to actually be typed instead of using the dreaded&nbsp;
+            <span className="bg-gray-700 font-mono">
+              {"{"} [x: any]: any; {"}"}
+            </span>
+            &nbsp; to define all fields.
+          </p>
+          <p>
+            Not only am I known at Lease End as the TypeScript guy but also the
+            &quot;review my PR&quot; guy. Everyone knows if they see that I am
+            reviewing their PR they&apos;re going to learn something. When I saw
+            &quot;You hold people to a high standard, and inspire them to do
+            their best work&quot; on the job description a light sparkled in my
+            eyes. As much as I love to help others learn and grow, I love it
+            even more when others help me to learn and grow as well.
+          </p>
+          <h3 className="text-xl py-2 text-gray-50">Rust</h3>
+          <p>
+            In my quest to learn new things, everywhere I looked, Rust was
+            staring back at me. I decided to pick it up and do a couple&nbsp;
+            <a
+              className="pointer-events-none lg:pointer-events-auto hover:text-gray-50 underline"
+              href="https://frontendmasters.com/"
+              target="_blank"
+              rel="noreferrer"
+            >
+              Front End Masters
+            </a>
+            &nbsp;courses from none other than&nbsp;
+            <a
+              className="pointer-events-none lg:pointer-events-auto hover:text-gray-50 underline"
+              href="https://twitter.com/ThePrimeagen"
+              target="_blank"
+              rel="noreferrer"
+            >
+              ThePrimeagen
+            </a>
+            &nbsp;(who also got me into VIM... but that&apos;s a story for
+            another time) and wrote an internal CLI tool in Rust for us
+            developers to speed up the some processes while developing locally.
+          </p>
+          <h4 className="text-lg py-2 text-gray-50">Current Project</h4>
+          <p>
+            As the AI hype train is still going strong I decided to hop aboard
+            and learn what&nbsp;
+            <a
+              className="pointer-events-none lg:pointer-events-auto hover:text-gray-50 underline"
+              href="https://ollama.com/"
+              target="_blank"
+              rel="noreferrer"
+            >
+              Ollama
+            </a>
+            &nbsp;is all about so I could test AI models locally. I&apos;ve been
+            working on an AI Photo Renamer desktop app using&nbsp;
+            <a
+              className="pointer-events-none lg:pointer-events-auto hover:text-gray-50 underline"
+              href="https://tauri.app/"
+              target="_blank"
+              rel="noreferrer"
+            >
+              Tauri
+            </a>
+            &nbsp;with React and&nbsp;
+            <a
+              className="pointer-events-none lg:pointer-events-auto hover:text-gray-50 underline"
+              href="https://ui.shadcn.com/"
+              target="_blank"
+              rel="noreferrer"
+            >
+              shadcn UI
+            </a>
+            &nbsp;on the front end with, of course, a Rust backend to hook into
+            Ollama. Check that out here!&nbsp;
+            <a
+              className="pointer-events-none lg:pointer-events-auto hover:text-gray-50 underline"
+              href="https://twitter.com/Elevated_SW/status/1772501048067772778"
+              target="_blank"
+              rel="noreferrer"
+            >
+              AI Photo Rename
+            </a>
+            .
+          </p>
+          <h3 className="text-xl py-2 text-gray-50">The End</h3>
+          <p>
+            Thank you for taking the time to read through all of this! I am
+            based in the Mountain Time Zone but am willing to work odd hours to
+            make it work. I also need to apologize for one more thing... A
+            couple years ago we were using styled-components at Lease End and I
+            convinced the teams to move away from it but I fought to move to
+            Chakra UI. This was before I knew what&nbsp;
+            <a
+              className="pointer-events-none lg:pointer-events-auto hover:text-gray-50 underline"
+              href="https://github.com/dcastil/tailwind-merge"
+              target="_blank"
+              rel="noreferrer"
+            >
+              Tailwind-Merge
+            </a>
+            &nbsp;was and I don&apos;t even think&nbsp;
+            <a
+              className="pointer-events-none lg:pointer-events-auto hover:text-gray-50 underline"
+              href="https://cva.style/docs"
+              target="_blank"
+              rel="noreferrer"
+            >
+              CVA
+            </a>
+            &nbsp; was a thing yet. I am sorry for my ignorance haha
+          </p>
         </div>
       </div>
     </main>
--- src/components/icons.tsx
+++ src/components/icons.tsx
@@ -9,26 +9,15 @@ export const XIcon = ({ size = 24, width, height, ...props }: IconSvgProps) => {
       xmlns="http://www.w3.org/2000/svg"
       fill="none"
       stroke="currentColor"
-      stroke-width="2"
-      stroke-linecap="round"
-      stroke-linejoin="round"
+      strokeWidth="2"
+      strokeLinecap="round"
+      strokeLinejoin="round"
       {...props}
     >
       <path stroke="none" d="M0 0h24v24H0z" fill="none" />
       <path d="M4 4l11.733 16h4.267l-11.733 -16z" />
       <path d="M4 20l6.768 -6.768m2.46 -2.46l6.772 -6.772" />
     </svg>
-    // <svg
-    // height={size || height}
-    // viewBox="0 0 24 24"
-    // width={size || width}
-    // {...props}
-    // >
-    //   <path
-    //     d="M19.633 7.997c.013.175.013.349.013.523 0 5.325-4.053 11.461-11.46 11.461-2.282 0-4.402-.661-6.186-1.809.324.037.636.05.973.05a8.07 8.07 0 0 0 5.001-1.721 4.036 4.036 0 0 1-3.767-2.793c.249.037.499.062.761.062.361 0 .724-.05 1.061-.137a4.027 4.027 0 0 1-3.23-3.953v-.05c.537.299 1.16.486 1.82.511a4.022 4.022 0 0 1-1.796-3.354c0-.748.199-1.434.548-2.032a11.457 11.457 0 0 0 8.306 4.215c-.062-.3-.1-.611-.1-.923a4.026 4.026 0 0 1 4.028-4.028c1.16 0 2.207.486 2.943 1.272a7.957 7.957 0 0 0 2.556-.973 4.02 4.02 0 0 1-1.771 2.22 8.073 8.073 0 0 0 2.319-.624 8.645 8.645 0 0 1-2.019 2.083z"
-    //     fill="currentColor"
-    //   />
-    // </svg>
   );
 };
 
