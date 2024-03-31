d7c1d48f8fa37ea4da5552b22f4af07a2827db94
Brennen Davis <brennendavis@gmail.com>
2024-03-30 23:32:31 -0600

    mobile screen

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -3,9 +3,9 @@ import Image from "next/image";
 
 export default function Home() {
   return (
-    <main className="flex min-h-screen flex-col items-center py-12">
-      <div className="flex flex-col gap-4 w-full max-w-3xl">
-        <div className="flex space-x-6">
+    <main className="flex min-h-screen flex-col items-center px-4 py-6 lg:py-12 lg:px-0">
+      <div className="flex flex-col gap-4 w-full lg:max-w-3xl">
+        <div className="flex space-x-2 lg:space-x-6">
           <div className="relative">
             <Image
               src="/me.jpg"
@@ -16,11 +16,11 @@ export default function Home() {
             />
             <div className="bg-violet-400 opacity-30 -translate-y-1 -translate-x-1 h-[128px] w-[136px] rounded-3xl"></div>
           </div>
-          <div className="flex flex-col space-y-4">
-            <h1 className="font-bold text-6xl">Brennen Davis</h1>
-            <div className="flex space-x-12">
+          <div className="flex flex-col lg:space-y-4">
+            <h1 className="font-bold text-4xl lg:text-6xl">Brennen Davis</h1>
+            <div className="flex space-x-3 lg:space-x-12">
               <a
-                className="text-gray-300 pointer-events-none flex place-items-center gap-2 p-8 lg:pointer-events-auto lg:p-0 hover:text-gray-50"
+                className="text-gray-300 pointer-events-none flex place-items-center gap-1 lg:gap-2  lg:pointer-events-auto lg:p-0 hover:text-gray-50"
                 href="https://github.com/BrennenRocks"
                 target="_blank"
                 rel="noreferrer"
@@ -29,7 +29,7 @@ export default function Home() {
                 <div>Github</div>
               </a>
               <a
-                className="text-gray-300 pointer-events-none flex place-items-center gap-2 p-8 lg:pointer-events-auto lg:p-0 hover:text-gray-50"
+                className="text-gray-300 pointer-events-none flex place-items-center gap-1 lg:gap-2 lg:pointer-events-auto lg:p-0 hover:text-gray-50"
                 href="https://twitter.com/Elevated_SW"
                 target="_blank"
                 rel="noreferrer"
@@ -198,7 +198,7 @@ export default function Home() {
               react-hook-form
             </a>
             .
-            <pre className="bg-gray-700 text-white break-all rounded-lg p-4 my-2">
+            <pre className="bg-gray-700 text-white break-all rounded-lg p-4 my-2 whitespace-pre-wrap">
               <div>
                 import {"{"} FieldValues, UseControllerProps,
                 useController&nbsp;
